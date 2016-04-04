9a4f612abed42595884b9f9b7340873d2c2f7810
demiurgosoft <demiurgosoft@hotmail.com>
2016-04-04 19:25:10 +0200

    signup #1

---
+++ public/scripts/signup.js
@@ -0,0 +1,42 @@
+//TODO: check email and password
+$(document).ready(function() {
+    $('#signup-form').submit(function(e) {
+        var postData = $(this).serializeArray();
+        var formURL = $(this).attr("action");
+        $.ajax({
+            url: formURL,
+            type: "POST",
+            data: postData,
+            dataType: 'json',
+            success: function(data, textStatus, jqXHR) {
+                console.log(data);
+                getDash(data.token);
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(JSON.stringify(jqXHR));
+                console.log(errorThrown);
+            }
+        });
+        e.preventDefault(); //STOP default action
+    });
+});
+
+
+function getDash(token) {
+    console.log("GET DASH");
+    $.ajax({
+        url: "http://localhost:8081/restricted/dash",
+        type: "GET",
+        headers: {
+            'Authorization': 'Bearer ' + token
+        },
+        success: function(data, status) {
+            return console.log("The returned data", data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(JSON.stringify(jqXHR));
+            console.log(errorThrown);
+        }
+    });
+
+}
