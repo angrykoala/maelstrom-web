7fa113b9ee7f8dcde5d386911014c2475072d361
demiurgosoft <demiurgosoft@hotmail.com>
2016-04-12 19:53:58 +0200

    Buy product

--- public/cargo.jsx
+++ public/cargo.jsx
@@ -46,7 +46,32 @@ var ProductDisplay = React.createClass({
     },
     buy: function(){
         var val=this.state.value || 0;
-        console.log("Buy "+val+" of "+this.props.name+" in "+this.props.shipId);
+        //console.log("Buy "+val+" of "+this.props.name+" in "+this.props.shipId);
+        if (User.logged() && val>0) {
+            var token = User.getToken();
+            $.ajax({
+                url: "http://localhost:8080/user/buy",
+                type: "POST",
+                dataType: 'json',
+                data:{
+                    ship:this.props.shipId,
+                    product: this.props.name,
+                    quantity: val
+                },
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                },
+                cache: false,
+                success: function(data) {
+                    console.log("SUCCESS "+data);
+                },
+                error: function(xhr, status, err) {
+                    console.log("ERROR "+err);
+                }
+            });
+        }
+        
+        
     },
     sell: function(){
         var val=this.state.value || 0;
