0dc2818612ced841941d04409f2ff3ecf08f33e5
demiurgosoft <demiurgosoft@hotmail.com>
2016-05-03 19:33:09 +0200

    env USERURL changed

--- config/urls.js
+++ config/urls.js
@@ -1,5 +1,5 @@
 module.exports = {
 	world: process.env.WORLDURL || "http://localhost:8080",
-	user: process.env.USERSURL || "http://localhost:8081",
+	user: process.env.USERURL || "http://localhost:8081",
 	web: process.env.WEBURL || "http://localhost:9090"
 };
